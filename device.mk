6b583dcdb4caeeadc44ecec76d8c5103bcd8425d
akifakif32 <mhmtkfdrn@gmail.com>
2026-02-03 02:50:43 +0300

    X6833B: default renderer now opengl and ota fixed

--- device.mk
+++ device.mk
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+LOCAL_PATH := device/infinix/X6833B
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
@@ -224,6 +226,25 @@ PRODUCT_PACKAGES += \
 # IMS
 $(call inherit-product, vendor/mediatek/ims/ims.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Project ID Quota
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    hardware/mediatek \
+    hardware/mediatek/libmtkperf_client \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/millennium
+
+>>>>>>> 14e947a (default renderer now opengl and ota fixed)
 # Init files
 PRODUCT_PACKAGES += \
     fstab.mt6789 \
