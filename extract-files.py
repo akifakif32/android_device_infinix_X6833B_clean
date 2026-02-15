63f34bb87367d82dfec1ce4f3c8e228641daa1dd
Apichat1234inwza007 <disom610@gmail.com>
2026-02-15 22:56:45 +0700

    X6833B: add stuff for X6833B

--- extract-files.py
+++ extract-files.py
@@ -15,7 +15,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/tecno/LH7n',
+    'device/infinix/X6833B',
     'hardware/mediatek',
     'hardware/mediatek/libmtkperf_client',
     'hardware/mediatek/libaedv',
@@ -110,7 +110,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'x6833b',
+    'X6833B',
     'infinix',
     blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
